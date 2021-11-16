35e741f997d7f3b7109d7567de073a003bf4340f
antonkasperi <daslo001@edu.xamk.fi>
2021-11-16 10:28:17 +0200

    Lisää harkkoja

---
+++ exercise6_5.py
@@ -0,0 +1,25 @@
+# sama lista
+products = ["T1565_2020", "T2432_2019",
+            "T8551_2018", "T3345_2019",
+            "Y51372_2019", "Y76715_2017",
+            "E98144_2018", "T7733_2020",
+            "E7614_2021", "E9722_2017",
+            "Y89875_2020", "T61287_2021",
+            "E9152_2019", "T7749_2021"]
+
+# pyydetään koodi str-muodossa
+query = input("Syötä tuotekoodi:\n")
+
+# haetaan silmukalla listasta
+for product in products:
+
+    # kun löytyy, käytetään .split-komentoa ja tulostetaan pelkkä vuosiluku
+    if query in product:
+        codeyear = product.split("_")
+        print(codeyear[1])
+
+        # ja kutsutaan break
+        break
+    # jos mitään ei löydy:
+    else:
+        print("Haulla 0 tulosta.")
