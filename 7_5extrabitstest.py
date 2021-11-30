834b0f16fc890496464df850ea2f59844a341271
antonkasperi <daslo001@edu.xamk.fi>
2021-11-30 13:09:20 +0200

    7_5 lisätehtävä

--- 7_5extrabitstest.py
+++ 7_5extrabitstest.py
@@ -1,6 +1,6 @@
 import json
 import urllib.request
-import var_dump as vd
+#import var_dump as vd
 
 url = "https://edu.frostbit.fi/api/weather/"
 req = urllib.request.Request(url)
@@ -13,18 +13,38 @@ corresponding_index_leastwindy = windspeeds.index(min(windspeeds))
 
 print(f"Tänään eniten tuulee sijainnissa: {weather[corresponding_index_windiest]['location']}, {max(windspeeds)} m/s")
 print(f"Tänään vähiten tuulee sijainnissa: {weather[corresponding_index_leastwindy]['location']}, {min(windspeeds)} m/s")
-
-vd.var_dump(weather)
+print()
+#vd.var_dump(weather)
 wind_lapland = []
 wind_middle = []
 wind_south = []
 
-for place in weather:
-    if 'area' == "lapland":
-        wind_lapland.append(place['wind'])
-    elif 'area' == "middle":
-        wind_middle.append(place['wind'])
-    elif 'area' == "south":
-        wind_south.append(place['wind'])
+for x in weather:
+    if x['area'] == 'lapland':
+        wind_lapland.append(x['wind'])
+    elif x['area'] == 'middle':
+        wind_middle.append(x['wind'])
+    elif x['area'] == 'south':
+        wind_south.append(x['wind'])
 
-print(wind_lapland)
+total_lapland = 0
+total_middle = 0
+total_south = 0
+for speed in wind_lapland:
+    total_lapland = total_lapland + speed
+    avgspeed_lapland = total_lapland / len(wind_lapland)
+    avgspeed_lapland = round(avgspeed_lapland, 2)
+
+for speed in wind_middle:
+    total_middle = total_middle + speed
+    avgspeed_middle = total_middle / len(wind_middle)
+    avgspeed_middle = round(avgspeed_middle, 2)
+
+for speed in wind_south:
+    total_south = total_south + speed
+    avgspeed_south = total_south / len(wind_south)
+    avgspeed_south = round(avgspeed_south, 2)
+
+print(f"Keskimääräinen tuuli, Lappi: {avgspeed_lapland} m/s")
+print(f"Keskimääräinen tuuli, Maan keskiosa: {avgspeed_middle} m/s")
+print(f"Keskimääräinen tuuli, Etelä-Suomi: {avgspeed_south} m/s")
