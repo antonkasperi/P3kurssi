6ba0801fa80a4a1a80bb0d5210cb6638504f5aa9
antonkasperi <daslo001@edu.xamk.fi>
2021-11-30 13:09:02 +0200

    7_5 lisätehtävä

--- exercise7_5.py
+++ exercise7_5.py
@@ -17,3 +17,44 @@ corresponding_index_leastwindy = windspeeds.index(min(windspeeds))
 # and the max/min values to get the float values
 print(f"Tänään eniten tuulee sijainnissa: {weather[corresponding_index_windiest]['location']}, {max(windspeeds)} m/s")
 print(f"Tänään vähiten tuulee sijainnissa: {weather[corresponding_index_leastwindy]['location']}, {min(windspeeds)} m/s")
+print()
+
+# lists for the separate wind speed values
+wind_lapland = []
+wind_middle = []
+wind_south = []
+
+# extraxting the wind speed values and assigning them into correct lists
+for x in weather:
+    if x['area'] == 'lapland':
+        wind_lapland.append(x['wind'])
+    elif x['area'] == 'middle':
+        wind_middle.append(x['wind'])
+    elif x['area'] == 'south':
+        wind_south.append(x['wind'])
+
+# defining the values for totals
+total_lapland = 0
+total_middle = 0
+total_south = 0
+
+# in a for-loop calculating the average wind speed for each area, rounding the result as requested
+for speed in wind_lapland:
+    total_lapland = total_lapland + speed
+    avgspeed_lapland = total_lapland / len(wind_lapland)
+    avgspeed_lapland = round(avgspeed_lapland, 2)
+
+for speed in wind_middle:
+    total_middle = total_middle + speed
+    avgspeed_middle = total_middle / len(wind_middle)
+    avgspeed_middle = round(avgspeed_middle, 2)
+
+for speed in wind_south:
+    total_south = total_south + speed
+    avgspeed_south = total_south / len(wind_south)
+    avgspeed_south = round(avgspeed_south, 2)
+
+# printing the results
+print(f"Keskimääräinen tuuli, Lappi: {avgspeed_lapland} m/s")
+print(f"Keskimääräinen tuuli, Maan keskiosa: {avgspeed_middle} m/s")
+print(f"Keskimääräinen tuuli, Etelä-Suomi: {avgspeed_south} m/s")
