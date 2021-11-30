c8df3e0e4dbe4a952f01a861c30bcef46c005635
antonkasperi <daslo001@edu.xamk.fi>
2021-11-30 13:29:57 +0200

    7_7 lisätehtävä

---
+++ exercise7_7.py
@@ -0,0 +1,11 @@
+import json
+import urllib.request
+from collections import Counter
+import var_dump as vd
+url = "https://liukastumisvaroitus-api.beze.io/api/v1/warnings"
+req = urllib.request.Request(url)
+raw_data = urllib.request.urlopen(req).read().decode("UTF-8")
+dontslip = json.loads(raw_data)
+
+#vd.var_dump(dontslip)
+Counter(dontslip)
